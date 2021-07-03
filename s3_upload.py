db85c4f794598547b89ce8de502487787053de63
raul-bermejo <raulbv.personal@gmail.com>
2021-07-03 16:22:52 +1200

    Updated s3_upload.py and created dl-sparkify bucket

--- s3_upload.py
+++ s3_upload.py
@@ -3,27 +3,35 @@ import json
 import configparser
 from botocore.exceptions import ClientError
 
+# Read AWS credentials from .cfg file
 config = configparser.ConfigParser()
 config.read_file(open('dl.cfg'))
 
-AWS_KEY_ID                    = config.get('AWS','AWS_KEY_ID')
-AWS_SECRET                 = config.get('AWS','AWS_SECRET')
+AWS_KEY_ID = config.get('AWS','AWS_KEY_ID')
+AWS_SECRET = config.get('AWS','AWS_SECRET')
 
+region = 'us-west-2'
 
-# Initialize AWS resources using AWS Python SDK (boto3)
-s3 = boto3.resource('s3',
-                    region_name='us-west-2',
-                    aws_access_key_id=AWS_KEY_ID,
-                    aws_secret_access_key=AWS_SECRET)
-
-
-# Extract list of current s3 buckets
+# Create Python client to use AWS API
 s3 = boto3.client('s3',
-                    region_name='us-west-2',
-                    aws_access_key_id=AWS_KEY_ID,
-                    aws_secret_access_key=AWS_SECRET)
+                  region_name=region,
+                  aws_access_key_id=AWS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET)
 
+# Create s3 bucket and upload zipped files from data dir (only once)
+create_bucket = False
+upload_data = False
+
+bucket_name = 'dl-sparkify'
+files = ['log-data.zip', 'song-data.zip']
+
+if create_bucket:
+    location = {'LocationConstraint': region}
+    s3.create_bucket(Bucket=f'{bucket_name}',
+                     CreateBucketConfiguration=location)
+
+
+# Print existing buckets
 response = s3.list_buckets()
-bucket_list = response["Buckets"]
-
+bucket_list = [bucket['Name'] for bucket in response["Buckets"]]
 print(bucket_list)
